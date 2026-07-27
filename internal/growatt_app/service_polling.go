4e1bac6034f69afb5c63d2c5e88309d6b36bfbc2
88zriw-eng <88zriw@gmail.com>
2026-07-27 13:10:14 +0200

    Update service_polling.go

--- internal/growatt_app/service_polling.go
+++ internal/growatt_app/service_polling.go
@@ -1,3 +1,10 @@
+package growatt_app
+
+import (
+	"log/slog"
+	"noah-mqtt/pkg/models"
+)
+
 func (g *GrowattAppService) pollStatus(device models.NoahDevicePayload) {
 	if data, err := g.client.GetNoahStatus(device.Serial); err != nil {
 		slog.Error(
@@ -6,7 +13,6 @@ func (g *GrowattAppService) pollStatus(device models.NoahDevicePayload) {
 			slog.String("device", device.Serial),
 		)
 	} else {
-
 		payload := devicePayload(data)
 
 		last := g.lastGenerationTotal[device.Serial]
@@ -14,6 +20,7 @@ func (g *GrowattAppService) pollStatus(device models.NoahDevicePayload) {
 		// Ungültige oder zurückspringende Gesamtenergie ignorieren
 		if last > 0 {
 			if payload.GenerationTotalEnergy <= 0 {
+
 				slog.Warn(
 					"Ignoring invalid GenerationTotalEnergy",
 					slog.String("device", device.Serial),
@@ -40,6 +47,41 @@ func (g *GrowattAppService) pollStatus(device models.NoahDevicePayload) {
 
 		for _, e := range g.endpoints {
 			e.PublishDeviceStatus(device, payload)
+		}
+	}
+}
+
+func (g *GrowattAppService) pollBatteryDetails(device models.NoahDevicePayload) {
+	if data, err := g.client.GetBatteryData(device.Serial); err != nil {
+		slog.Error(
+			"could not get battery data",
+			slog.String("error", err.Error()),
+			slog.String("device", device.Serial),
+		)
+	} else {
+		var batteryPayloads []models.BatteryPayload
+
+		for _, bat := range data.Obj.Batter {
+			batteryPayloads = append(batteryPayloads, batteryPayload(&bat))
+		}
+
+		for _, e := range g.endpoints {
+			e.PublishBatteryDetails(device, batteryPayloads)
+		}
+	}
+}
+
+func (g *GrowattAppService) pollParameterData(device models.NoahDevicePayload) {
+	if data, err := g.client.GetNoahInfo(device.Serial); err != nil {
+		slog.Error(
+			"could not get parameter data",
+			slog.String("error", err.Error()),
+			slog.String("device", device.Serial),
+		)
+	} else {
+		payload := parameterPayload(data)
+		for _, e := range g.endpoints {
+			e.PublishParameterData(device, payload)
 		}
 	}
 }
